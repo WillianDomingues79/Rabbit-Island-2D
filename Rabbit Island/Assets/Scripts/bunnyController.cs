31c141ef11fc69e1d9cfaf334b7eb17e9e1dc058
Willian Domingues <willian.domingues3@gmail.com>
2023-12-07 09:49:08 -0300

    Correcao pulo

--- Rabbit Island/Assets/Scripts/bunnyController.cs
+++ Rabbit Island/Assets/Scripts/bunnyController.cs
@@ -6,16 +6,22 @@ public class bunnyController : MonoBehaviour
 {
     public float forcaPulo;
     private Rigidbody2D playerRb;
+    private bool grounded;
+    public Transform groundCheck;
     
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
     }
 
+    void FixedUpdate(){
+        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+    }
+
     
     void Update()
     {
-        if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump") && grounded == true){
            playerRb.AddForce(new Vector2(0, forcaPulo));     
         }
         
